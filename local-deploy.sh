a65e18afbc3a8fd815ecf6ecdfe059ad58ed9159
Allan Rehhoff~ <allan.rehhof@gmail.com>
2023-01-25 23:16:25 +0100

    Update deploy script

--- local-deploy.sh
+++ local-deploy.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+echo "[INFO] Changing working directory";
+cd "$(dirname "$0")"
+
 echo "[INFO] Pulling updates from origin/master";
 git fetch --quiet origin master
 git reset --quiet --hard origin/master
-git clean -d -f --exclude local-deploy.sh
 
 echo "[INFO] Installing npm dependencies";
 npm install --silent --omit=dev &>/dev/null
+
+echo "[INFO] Adding exeution permissions to deploy scxript"
+chmod +x ./local-deploy.sh
 
 echo "[INFO] Deploying complete";
