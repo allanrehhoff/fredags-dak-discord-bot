72205c5db5d6e1967accd99608b3ff69ca8bde77
Allan Rehhoff~ <allan.rehhof@gmail.com>
2023-01-25 23:18:36 +0100

    Git clean with more args

--- local-deploy.sh
+++ local-deploy.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")"
 echo "[INFO] Pulling updates from origin/master";
 git fetch --quiet origin master
 git reset --quiet --hard origin/master
-git clean -d
+git clean -d -f
 
 echo "[INFO] Installing npm dependencies";
 npm install --silent --omit=dev &>/dev/null
